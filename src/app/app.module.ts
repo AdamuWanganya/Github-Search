f53b96fad01ff5baafcef7aa828e78d8ace1c38c
AdamuWanganya <wanganyaadamu@gmail.com>
2021-11-30 01:27:39 +0300

    Edit README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { ProfileService } from './profile.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
-import { HighlightDirective } from './highlight.directive';
-import { DateCountPipe } from './date-count.pipe';
+
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileService } from './profile.service';
+import { HighlightDirective } from './highlight.directive';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    NavbarComponent,
     HighlightDirective,
-    DateCountPipe,
-    NavbarComponent
+    AboutComponent,    
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    
+   
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
